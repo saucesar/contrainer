ebdc1ebb89c3c6890f0bed781d2de1ac8c5c9992
saucesar <sau.cesarlima2013@gmail.com>
2020-03-25 20:43:03 -0300

    Criação de usuario padrao no banco de dados.

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class DatabaseSeeder extends Seeder
 {
@@ -12,5 +13,13 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UsersTableSeeder::class);
+        DB::table('users')->insert([
+            'nome'         => 'Admin  admin',
+            'email'        => 'admin@nuvem.com',
+            'password'     => bcrypt('123456'),
+            'tipo_usuario' => 'admin',
+            'created_at'   => now(),
+            'updated_at'   => now()
+        ]);
     }
 }
