4fe50a4dcc82c04e138ab7d91bff045a06d8d216
saucesar <sau.cesarlima2013@gmail.com>
2020-03-25 20:40:37 -0300

    criadas rotas de login.

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,5 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/','Controller@login')->name('user.login');
+Route::post('/','Controller@autenticar')->name('user.auth');
