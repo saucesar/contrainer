7e6d4c94dfd0a6744f0d52d8b072460ac230390a
saucesar <sau.cesarlima2013@gmail.com>
2020-03-26 18:08:54 -0300

    Criada estrutura das views para crud usuário.

---
+++ resources/views/users/create.blade.php
@@ -0,0 +1,6 @@
+@extends('templates.base-template')
+
+@section('content')
+    <h1>Novo Usuário</h1>
+    @include('form')
+@endsection
---
+++ resources/views/users/edit.blade.php
@@ -0,0 +1,6 @@
+@extends('templates.base-template')
+
+@section('content')
+    <h1>Atualização de Usuário</h1>
+    @include('form')
+@endsection
