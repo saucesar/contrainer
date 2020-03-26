e84eda1d09e1ee7a0f657668ebfb97f9eb6b2890
saucesar <sau.cesarlima2013@gmail.com>
2020-03-26 20:33:27 -0300

    Criado model para maquina.

---
+++ app/Models/Maquina.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Maquina extends Model
+{
+    use SoftDeletes;
+
+    protected $fillable = ['cpu_utilizavel', 'ram_utilizavel'];
+
+    public static $rules = [
+        'cpu_utilizavel' => ['required', 'numeric', 'between:1,100',],
+        'ram_utilizavel' => ['required', 'numeric', 'min:128',],
+    ];
+
+    public static $messages = [
+        'required' => 'O campo :attribute é obrigatório',
+        'numeric'  => 'O campor :attribute é numérico',
+        'min'      => 'Quantidade de :attribute informada está abaixo do permitido.'
+    ];
+}
---
+++ database/migrations/2020_03_26_185635_create_maquinas_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMaquinasTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('maquinas', function (Blueprint $table) {
+            $table->id();
+            $table->integer('cpu_utilizavel')->default(25);// percentual(%)
+            $table->integer('ram_utilizavel');// em MB
+            $table->string('hashcode');// poderia ser o MAC?
+            $table->boolean('disponivel')->default(false);
+            $table->bigInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('maquinas');
+    }
+}
