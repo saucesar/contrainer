ca50754a621c18983e6e4669a601e1a2809b8840
saucesar <sau.cesarlima2013@gmail.com>
2020-03-31 17:00:33 -0300

    Validaçao para campos de user.

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,7 +2,7 @@
 
 namespace App\Http\Controllers;
 
-use App\User;
+use App\Models\User;
 use App\Http\Requests\UserRequest;
 use Illuminate\Support\Facades\Hash;
 
@@ -17,5 +17,92 @@ class UserController extends Controller
     public function index(User $model)
     {
         return view('users.index', ['users' => $model->paginate(15)]);
+    }
+
+      /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('users.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(UserRequest $request)
+    {
+        $this->validate($request, $request->rules());
+
+        $user = User::create($request->all());
+
+        return view('users.login');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        return view('users.show', ['user' => User::where('id', $id)->first()]);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        return view('users.edit', ['user' => User::where('id', $id)->first()]);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(UserRequest $request, $id)
+    {
+        $this->validate($request, $request->rules());
+
+        $user = User::firstWhere('id',$id);
+        $result = $user->update($request->all());
+
+        if($result) {
+            return redirect()->route('home.index');
+        } else {
+            return redirect()->back()->withInput();
+        }
+
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $user = User::firstWhere('id',$id);
+        $result = $user->delete();
+
+        if($result) {
+            return redirect()->route('home.index');
+        } else {
+            return redirect()->back();
+        }
     }
 }
