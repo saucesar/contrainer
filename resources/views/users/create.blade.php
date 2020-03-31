1f672a27e10c798d83428e53aef13c35a1470a8b
saucesar <sau.cesarlima2013@gmail.com>
2020-03-31 16:58:06 -0300

    Deleção de iten unitilizados.

--- resources/views/users/create.blade.php
+++
@@ -1,6 +0,0 @@
-@extends('templates.base-template')
-
-@section('content')
-    <h1>Novo Usuário</h1>
-    @include('form')
-@endsection
--- resources/views/users/edit.blade.php
+++
@@ -1,6 +0,0 @@
-@extends('templates.base-template')
-
-@section('content')
-    <h1>Atualização de Usuário</h1>
-    @include('form')
-@endsection
