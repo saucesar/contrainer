e7966100a5d494fcb06982a2f03994cca6927b97
saucesar <sau.cesarlima2013@gmail.com>
2020-03-31 17:01:23 -0300

    Metodos de validacao movidos para User Request.

--- app/Models/User.php
+++ app/Models/User.php
@@ -5,7 +5,6 @@ namespace App\Models;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
-use App\Models\Telefone;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
 class User extends Authenticatable
@@ -33,23 +32,6 @@ class User extends Authenticatable
      * @var array
      */
     protected $casts = ['email_verified_at' => 'datetime'];
-
-    public static $rules = [
-        'name'         => ['required', 'min:3'],
-        'email'        => ['required', 'unique:users'],
-        'password'     => ['required', 'min:8']
-    ];
-
-    public static $messages = [
-        'required'=> 'O campo :attribute é obrigatório.',
-        'unique'  => 'O :attribute informado já possui cadastro.',
-        'min'     => 'Quantidade menor que o permitido no campo :attribute.'
-    ];
-
-    public function telefone()
-    {
-        return $this->hasOne(Telefone::class);
-    }
 
     public function maquinas()
     {
