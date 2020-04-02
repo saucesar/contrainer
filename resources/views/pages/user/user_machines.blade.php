18a1a3f812373eb8be5064c0527a3d30f2f2913e
saucesar <sau.cesarlima2013@gmail.com>
2020-04-02 03:47:02 -0300

    Telas de cadastro, edicao e lista de maquinas.

---
+++ resources/views/pages/user/user_machines.blade.php
@@ -0,0 +1,61 @@
+@extends('layouts.app', ['activePage' => 'user-machines', 'titlePage' => __("User's Machines")])
+
+@section('content')
+<div class="content">
+    <div class="container-fluid">
+      <div class="row">
+        <div class="col-md-12">
+          <div class="card">
+            <div class="card-header card-header-primary">
+              <h4 class="card-title ">Machines Table</h4>
+              <p class="card-category">List of {{ $user_name }} machines</p>
+            </div>
+            <div class="card-body">
+              <div class="table-responsive">
+                  <a href="{{ route('machines.create') }}">
+                      <i class="material-icons">queue</i>
+                      Add New
+                  </a>
+                  @if(session('error'))
+                    <div class="alert alert-danger">{{ session('error') }}</div>
+                  @endif
+                  <table class='table'>
+                      <thead>
+                          <th>Id</th>
+                          <th>CPU Available(%)</th>
+                          <th>RAM Available(Mb)</th>
+                          <th>Available(Now)</th>
+                          <th>Options</th>
+                      </thead>
+                      <tbody>
+                          @foreach ($machines as $machine)
+                            <tr>
+                                <td>{{ $machine->id }}</td>
+                                <td>{{ $machine->cpu_utilizavel }}</td>
+                                <td>{{ $machine->ram_utilizavel }}</td>
+                                <td>{{ $machine->disponivel?'Yes':'No' }}</td>
+                                <td>
+                                  <div class='content'>
+                                    <div class='conteiner-fluid'>
+                                      <div class='row'>
+                                        <a href="{{ route('machines.show', $machine) }}" class="btn btn-sm btn-outline-info"><i class="material-icons">remove_red_eye</i></a>  
+                                        <a href="{{ route('machines.edit', $machine) }}" class="btn btn-sm btn-outline-warning"><i class="material-icons">create</i></a>
+                                        {!! Form::open(['route' => ['machines.destroy', $machine], 'method' => 'delete']) !!}
+                                          <button onclick="return confirm('Are you sure?')" type="submit" class="btn btn-sm btn-outline-danger"><i class="material-icons">delete_sweep</i></button>
+                                        {!! Form::close() !!}                                      </div>
+                                    </div>
+                                  </div>
+
+                                </td>
+                            </tr>                          
+                          @endforeach
+                      </tbody>
+                  </table>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+@endsection
---
+++ resources/views/pages/user/user_machines_new.blade.php
@@ -0,0 +1,34 @@
+@extends('layouts.app', ['activePage' => 'user-machines', 'titlePage' => __("User's Machines")])
+
+@section('content')
+<div class="content">
+    <div class="container-fluid">
+      <div class="row">
+        <div class="col-md-12">
+          <div class="card">
+            <div class="card-header card-header-primary">
+              <h4 class="card-title ">New Machine</h4>
+              <p class="card-category">new machine register</p>
+            </div>
+            <div class="card-body">
+              <div class="table-responsive">
+                  @if($errors->any())
+                    <div class="alert alert-danger">
+                      <ul>
+                          @foreach ($errors->all() as $error)
+                              <li>{{$error}}</li>
+                          @endforeach
+                      </ul>
+                    </div>
+                  @endif
+                  {!! Form::open(['route' => 'machines.store', 'method' => 'post']) !!}
+                    @include('pages.user.machine_form',['selected_cpu' => null, 'selected_ram' => null])
+                  {!! Form::close() !!}
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+@endsection
