01a029ce8be34397217f12bc1dd0a9cf746715ea
saucesar <sau.cesarlima2013@gmail.com>
2020-04-02 03:48:10 -0300

    correcao de nome controller de rota instanciaC...

--- routes/api.php
+++ routes/api.php
@@ -19,5 +19,5 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 });
 
 Route::apiResource('atividadeMaquinas','AtividadeMaquinasController');
-Route::apiResource('InstanciaContainers','InstanciaContainersController');
+Route::apiResource('InstanciaContainers','InstanciaContainerController');
 
