f5f6de71565ad65fd73e6c739bf3280d7c253aa1
saucesar <sau.cesarlima2013@gmail.com>
2020-04-02 03:42:49 -0300

    Limpesa de condigo unutilizado.

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,38 +3,30 @@
 namespace App\Http\Controllers;
 
 use App\Models\User;
+use App\Models\Maquina;
+use Illuminate\Support\Facades\Auth;
 use App\Http\Requests\UserRequest;
-use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller
 {
-    /**
-     * Display a listing of the users
-     *
-     * @param  \App\User  $model
-     * @return \Illuminate\View\View
-     */
+    public function machines()
+    {
+        $user = Auth::user();
+        $machines = Maquina::where('user_id', $user->id)->orderby('id')->get();
+
+		return view('pages.user.user_machines', ['machines' => $machines, 'user_name' => $user->name]);
+    }
+
     public function index(User $model)
     {
         return view('users.index', ['users' => $model->paginate(15)]);
     }
 
-      /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
     public function create()
     {
         return view('users.create');
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
     public function store(UserRequest $request)
     {
         $this->validate($request, $request->rules());
@@ -44,35 +36,16 @@ class UserController extends Controller
         return view('users.login');
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function show($id)
     {
         return view('users.show', ['user' => User::where('id', $id)->first()]);
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function edit($id)
     {
         return view('users.edit', ['user' => User::where('id', $id)->first()]);
     }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function update(UserRequest $request, $id)
     {
         $this->validate($request, $request->rules());
@@ -88,12 +61,6 @@ class UserController extends Controller
 
     }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function destroy($id)
     {
         $user = User::firstWhere('id',$id);
