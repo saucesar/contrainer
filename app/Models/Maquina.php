426b43982c7f0c0f6e9444e0526395e3879bf2cf
saucesar <sau.cesarlima2013@gmail.com>
2020-04-02 03:44:27 -0300

    Validações realocadas na classe MachineRequest.

--- app/Models/Maquina.php
+++ app/Models/Maquina.php
@@ -10,18 +10,7 @@ class Maquina extends Model
 {
     use SoftDeletes;
 
-    protected $fillable = ['cpu_utilizavel', 'ram_utilizavel'];
-
-    public static $rules = [
-        'cpu_utilizavel' => ['required', 'numeric', 'between:1,100',],
-        'ram_utilizavel' => ['required', 'numeric', 'min:128',],
-    ];
-
-    public static $messages = [
-        'required' => 'O campo :attribute é obrigatório',
-        'numeric'  => 'O campor :attribute é numérico',
-        'min'      => 'Quantidade de :attribute informada está abaixo do permitido.'
-    ];
+    protected $fillable = ['cpu_utilizavel', 'ram_utilizavel', 'hashcode', 'user_id', 'disponivel'];
 
     public function user():User
     {
