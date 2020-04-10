12a6875a00efc240350012b4c6a15a21c3b0961f
saucesar <sau.cesarlima2013@gmail.com>
2020-04-10 16:38:47 -0300

    Direcionamento de view index correta.

--- app/Http/Controllers/ContainersController.php
+++ app/Http/Controllers/ContainersController.php
@@ -8,32 +8,17 @@ use App\Models\Container;
 
 class ContainersController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
+
     public function index()
     {
-        //
+        return view('pages/containers/containers',['containers' => Container::all()]);
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
     public function create()
     {
         return view('containers.create',['programas' => $this->getProgramsList()]);
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
     public function store(Request $request)
     {
         $this->validar($request);
@@ -44,35 +29,16 @@ class ContainersController extends Controller
         Container::create($dados);
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function show($id)
     {
         return view('containers.show',['container' => Container::firstWhere('id', $id)]);
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function edit($id)
     {
         return view('containers.edit',['container' => Container::firstWhere('id', $id)]);   
     }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function update(Request $request, $id)
     {
         $this->validar($request);
@@ -84,12 +50,6 @@ class ContainersController extends Controller
         $container->update($dados);
     }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function destroy($id)
     {
         $container = Container::firstWhere('id', $id);
