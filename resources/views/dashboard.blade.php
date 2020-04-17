01be9a2ed5d399fc7fac38e688f6f1c9b2925c14
saucesar <sau.cesarlima2013@gmail.com>
2020-04-16 22:57:21 -0300

    Exibição de horas atividade de todas as maquinas.

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -8,17 +8,16 @@
           <div class="card card-stats">
             <div class="card-header card-header-warning card-header-icon">
               <div class="card-icon">
-                <i class="material-icons">content_copy</i>
+                <i class="material-icons">access_time</i>
               </div>
-              <p class="card-category">Used Space</p>
-              <h3 class="card-title">49/50
-                <small>GB</small>
+              <p class="card-category">Total Machine Uptime</p>
+              <h3 class="card-title">{{ $totalTime ?? null }}
+                <small>hours</small>
               </h3>
             </div>
             <div class="card-footer">
               <div class="stats">
-                <i class="material-icons text-danger">warning</i>
-                <a href="#pablo">Get More Space...</a>
+                <i class="material-icons">access_time</i>Current time
               </div>
             </div>
           </div>
