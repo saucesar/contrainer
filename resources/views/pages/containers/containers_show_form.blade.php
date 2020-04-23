82f0fa2a1e7626a6182654b68ca4282e751f3666
saucesar <sau.cesarlima2013@gmail.com>
2020-04-23 14:17:19 -0300

    Tela de exibição de dados de container.

---
+++ resources/views/pages/containers/containers_show_form.blade.php
@@ -0,0 +1,8 @@
+<p><label> Programs List :</label> {{ $container->programs }}</p>
+@if ($isAdmin)
+    <p><label> Command pull:</label> {{ $container->command_pull }}</p>
+    <p><label> Command run :</label> {{ $container->command_run }}</p>
+    <p><label> Created at  :</label> {{ $container->created_at }}</p>
+    <p><label> Updated at  :</label> {{ $container->updated_at }}</p>
+    <p><label> Updated at  :</label> {{ $container->updated_at }}</p>
+@endif
