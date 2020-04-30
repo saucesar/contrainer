1e0f1deac012d1aeb12be6845b929ae50dd712d6
saucesar <sau.cesarlima2013@gmail.com>
2020-04-30 00:56:04 -0300

    rota para parar um container.

--- routes/api.php
+++ routes/api.php
@@ -20,3 +20,4 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 
 Route::apiResource('atividadeMaquinas','Api\AtividadeMaquinasController');
 Route::apiResource('InstanciaContainers','Api\InstanciaContainerController');
+Route::get('InstanciaContainers/stop/{containerId}','Api\InstanciaContainerController@stop')->name('instance.stop');
