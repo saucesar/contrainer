6e11a7599aed88ae607615fd57e60d97d219463f
saucesar <sau.cesarlima2013@gmail.com>
2020-05-19 22:59:11 -0300

    Tela de area para admin de sistema

---
+++ resources/views/pages/admin/index.blade.php
@@ -0,0 +1,166 @@
+@extends('layouts.app', ['activePage' => 'admin-area', 'titlePage' => __("Admin Area")])
+
+@section('content')
+  <div class="content">
+    <div class="container-fluid">
+      <div class="row">
+        <div class="col-lg-3 col-md-6 col-sm-6">
+          <div class="card card-stats">
+            <div class="card-header card-header-info card-header-icon">
+              <div class="card-icon">
+                <i class="material-icons">laptop</i>
+              </div>
+              <p class="card-category">Registered Machines</p>
+              <h3 class="card-title">{{ $numberOfMach }}</h3>
+              <div class="collapse card-title" id="machine-details">
+                <br>
+                <p>Active: {{ $inActivity }}</p>
+              </div>
+            </div>
+            <div class="card-footer">
+                <a rel="tooltip" class="btn btn-link" data-toggle="collapse" data-target="#machine-details" aria-expanded="false" aria-controls="collapseExample">
+                    <i class="material-icons ">expand_more</i>
+                    More Details
+                </a>
+            </div>
+          </div>
+        </div>
+
+        <div class="col-lg-3 col-md-6 col-sm-6">
+          <div class="card card-stats">
+            <div class="card-header card-header-info card-header-icon">
+              <div class="card-icon">
+                <i class="material-icons">album</i>
+              </div>
+              <p class="card-category">Container Images</p>
+              <h3 class="card-title">{{ $numberOfCont }}</h3>
+              <div class="collapse card-title" id="container-details">
+                  <table class='table'>
+                      <thead>
+                          <th>Image</th>
+                          <th>Instances</th>
+                      </thead>
+                      <tbody>
+                          @foreach($containers as $container)
+                          <tr>
+                              <td>{{ $container->name }}</td>
+                              <td>{{ $instacesOfeachImage[$container->id] }}</td>
+                          </tr>
+                          @endforeach
+                          
+                      </tbody>
+                  </table>
+              </div>
+            </div>
+            <div class="card-footer">
+                <a rel="tooltip" class="btn btn-link" data-toggle="collapse" data-target="#container-details" aria-expanded="false" aria-controls="collapseExample">  
+                    <i class="material-icons">expand_more</i>
+                    More Details
+                </a>
+            </div>
+          </div>
+        </div>
+
+        <div class="col-lg-3 col-md-6 col-sm-6">
+          <div class="card card-stats">
+            <div class="card-header card-header-danger card-header-icon">
+              <div class="card-icon">
+                <i class="material-icons">info_outline</i>
+              </div>
+              <p class="card-category">Fixed Issues</p>
+              <h3 class="card-title">75</h3>
+            </div>
+            <div class="card-footer">
+              <div class="stats">
+                <i class="material-icons">local_offer</i> Tracked from Github
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div class="col-lg-3 col-md-6 col-sm-6">
+          <div class="card card-stats">
+            <div class="card-header card-header-info card-header-icon">
+              <div class="card-icon">
+                <i class="fa fa-twitter"></i>
+              </div>
+              <p class="card-category">Followers</p>
+              <h3 class="card-title">+245</h3>
+            </div>
+            <div class="card-footer">
+              <div class="stats">
+                <i class="material-icons">update</i> Just Updated
+              </div>
+            </div>
+          </div>
+        </div>
+
+      </div>
+
+      <div class="row">
+
+        <div class="col-md-4">
+          <div class="card card-chart">
+            <div class="card-header card-header-success">
+              <div class="ct-chart" id="dailySalesChart"></div>
+            </div>
+            <div class="card-body">
+              <h4 class="card-title">Daily Sales</h4>
+              <p class="card-category">
+                <span class="text-success"><i class="fa fa-long-arrow-up"></i> 55% </span> increase in today sales.</p>
+            </div>
+            <div class="card-footer">
+              <div class="stats">
+                <i class="material-icons">access_time</i> updated 4 minutes ago
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div class="col-md-4">
+          <div class="card card-chart">
+            <div class="card-header card-header-warning">
+              <div class="ct-chart" id="websiteViewsChart"></div>
+            </div>
+            <div class="card-body">
+              <h4 class="card-title">Email Subscriptions</h4>
+              <p class="card-category">Last Campaign Performance</p>
+            </div>
+            <div class="card-footer">
+              <div class="stats">
+                <i class="material-icons">access_time</i> campaign sent 2 days ago
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div class="col-md-4">
+          <div class="card card-chart">
+            <div class="card-header card-header-danger">
+              <div class="ct-chart" id="completedTasksChart"></div>
+            </div>
+            <div class="card-body">
+              <h4 class="card-title">Completed Tasks</h4>
+              <p class="card-category">Last Campaign Performance</p>
+            </div>
+            <div class="card-footer">
+              <div class="stats">
+                <i class="material-icons">access_time</i> campaign sent 2 days ago
+              </div>
+            </div>
+          </div>
+        </div>
+
+      </div>
+    </div>
+  </div>
+@endsection
+
+@push('js')
+  <script>
+    $(document).ready(function() {
+      // Javascript method's body can be found in assets/js/demos.js
+      md.initDashboardPageCharts();
+    });
+  </script>
+@endpush
