ca4ef5f6cc4f87f7d4d794841a4b1419414a58ad
saucesar <sau.cesarlima2013@gmail.com>
2020-05-21 00:26:05 -0300

    autorizações para areas de admin.

--- app/Http/Controllers/AdminAreaController.php
+++ app/Http/Controllers/AdminAreaController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers;
 use App\Models\Container;
 use App\Models\InstanciaContainer;
 use App\Models\Maquina;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
 
 class AdminAreaController extends Controller
 {
@@ -12,12 +14,63 @@ class AdminAreaController extends Controller
     {
         $params = [
             'numberOfMach' => Maquina::all()->count(),
+            'inActivity' => Maquina::where('disponivel', true)->get()->count(),
             'numberOfCont' => Container::all()->count(),
-            'containers'   => Container::all(),
+            'containers' => Container::all(),
+            'numberOfUsers' => User::all()->count(),
+            'registeredToday' => User::whereYear('created_at', date('Y'))->whereMonth('created_at', date('m'))->whereDay('created_at', date('d'))->count(),
+            'registeredMonth' => User::whereYear('created_at', date('Y'))->whereMonth('created_at', date('m'))->count(),
             'instacesOfeachImage' => $this->getInstacesOfEachImage(),
-            'inActivity'   => Maquina::where('disponivel', true)->get()->count()
         ];
+
         return view('pages/admin/index', $params);
+    }
+
+    public function machines()
+    {
+        if (Auth::user()->isAdmin()) {
+            return view('pages/admin/machines', ['machines' => Maquina::all()]);
+        } else {
+            return redirect()->back()->with('error', 'User not Authorized!!!');
+        }
+    }
+
+    public function users()
+    {
+        if (Auth::user()->isAdmin()) {
+            $users = User::all()->sortBy('id');
+            $params = [
+                'users' => $users,
+                'machinesCount' => $this->getMachinesCount($users),
+                'containersCount' => $this->getInstanceContainersCount($users),
+            ];
+
+            return view('pages/admin/users', $params);
+        } else {
+            return redirect()->back()->with('error', 'User not Authorized!!!');
+        }
+    }
+
+    public function getMachinesCount($users)
+    {
+        $array = [];
+
+        foreach ($users as $user) {
+            $array[$user->id] = Maquina::where('user_id', $user->id)->count();
+        }
+
+        return $array;
+    }
+
+    public function getInstanceContainersCount($users)
+    {
+        $array = [];
+
+        foreach ($users as $user) {
+            $array[$user->id] = InstanciaContainer::where('user_id', $user->id)->count();
+        }
+
+        return $array;
     }
 
     private function getInstacesOfEachImage()
@@ -25,12 +78,11 @@ class AdminAreaController extends Controller
         $containers = Container::all();
 
         $array = [];
-        
-        foreach($containers as $container){
+
+        foreach ($containers as $container) {
             $array[$container->id] = InstanciaContainer::where('image_id', $container->id)->get()->count();
         }
 
         return $array;
-
     }
 }
