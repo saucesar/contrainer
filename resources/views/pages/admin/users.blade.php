4d0575c9867d106e48bc6558908f83d549bc0e89
saucesar <sau.cesarlima2013@gmail.com>
2020-05-21 00:19:45 -0300

    Tela de visualização de usuario cadastrados

---
+++ resources/views/pages/admin/users.blade.php
@@ -0,0 +1,44 @@
+@extends('layouts.app', ['activePage' => 'admin-area', 'titlePage' => __("Admin Area")])
+
+@section('content')
+<div class="content">
+  <div class="container-fluid">
+    <div class="row">
+      <div class="col-md-12">
+        <div class="card">
+          <div class="card-header card-header-primary">
+            <h4 class="card-title ">Machines List</h4>
+            <p class="card-category">List of registered machines</p>
+          </div>
+          <div class="card-body">
+            <div class="table-responsive">
+              <table class='table'>
+                <thead>
+                    <th>#</th>
+                    <th>Name</th>
+                    <th>Email</th>
+                    <th>Type</th>
+                    <th>Machines</th>
+                    <th>Containers</th>
+                </thead>
+                <tbody>
+                    @foreach($users as $user)
+                        <tr>
+                            <td>{{ $user->id }}</td>
+                            <td>{{ $user->name }}</td>
+                            <td>{{ $user->email }}</td>
+                            <td>{{ $user->user_type }}</td>
+                            <td>{{ $machinesCount[$user->id]}}</td>
+                            <td>{{ $containersCount[$user->id] }}</td>
+                        </tr>
+                    @endforeach
+                </tbody>
+              </table>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+@endsection
