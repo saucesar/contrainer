c457e13233cb0a1e61ea7705921e112f86617c05
saucesar <sau.cesarlima2013@gmail.com>
2020-05-21 22:36:49 -0300

    Modificação nos cards

--- resources/views/pages/admin/index.blade.php
+++ resources/views/pages/admin/index.blade.php
@@ -123,12 +123,12 @@
               <div class="ct-chart" id="websiteViewsChart"></div>
             </div>
             <div class="card-body">
-              <h4 class="card-title">Email Subscriptions</h4>
+              <h4 class="card-title">User Subscriptions</h4>
               <p class="card-category">Last Campaign Performance</p>
             </div>
             <div class="card-footer">
               <div class="stats">
-                <i class="material-icons">access_time</i> campaign sent 2 days ago
+                <i class="material-icons">access_time</i> campaign
               </div>
             </div>
           </div>
