14aab209d4a02791c62d748015b40ba8d3c86219
saucesar <sau.cesarlima2013@gmail.com>
2020-05-22 22:07:37 -0300

    Adicao de componentes tabela.

--- resources/views/pages/admin/users.blade.php
+++ resources/views/pages/admin/users.blade.php
@@ -12,29 +12,7 @@
                     </div>
                     <div class="card-body">
                         <div class="table-responsive">
-                            <table class='table'>
-                                <thead>
-                                    <th>#</th>
-                                    <th>Name</th>
-                                    <th>Email</th>
-                                    <th>Type</th>
-                                    <th>Machines</th>
-                                    <th>Containers</th>
-                                </thead>
-                                <tbody>
-                                    @foreach($users as $user)
-                                    <tr>
-                                        <td>{{ $user->id }}</td>
-                                        <td>{{ $user->name }}</td>
-                                        <td>{{ $user->email }}</td>
-                                        <td>{{ $user->user_type }}</td>
-                                        <td>{{ $machinesCount[$user->id]}}</td>
-                                        <td>{{ $containersCount[$user->id] }}</td>
-                                    </tr>
-                                    @endforeach
-                                </tbody>
-                            </table>
-                            {!! $users->links() !!}
+                            @include('pages/tables/users_table', ['users' => $users]);
                         </div>
                     </div>
                 </div>
