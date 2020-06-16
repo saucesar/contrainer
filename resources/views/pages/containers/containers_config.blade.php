4990ebdb048e7b812c2f07d2327bc6f7789989a0
saucesar <sau.cesarlima2013@gmail.com>
2020-06-16 01:15:04 -0300

    Adicao de parametros para criar containers.

--- resources/views/pages/containers/containers_config.blade.php
+++ resources/views/pages/containers/containers_config.blade.php
@@ -22,9 +22,10 @@
                             <div class="alert alert-success">{{ session('success') }}</div>
                             @endif
                             {!! Form::open(['route' => 'InstanciaContainers.store', 'method' => 'post']) !!}
-                            <input type="hidden" value="{{ $container->id }}" name='image_id'>
-                            <input type="hidden" value="{{ $userId }}" name='user_id'>
-                            <h4 class="card-title">Image Selected : {{ $container->name }}</h4>
+                            <input type="hidden" value="{{ $image->name }}" name='Image'>
+                            <input type="hidden" value="{{ $image->id }}" name='image_id'>
+                            <input type="hidden" value="{{ $user_id }}" name='user_id'>
+                            <h4 class="card-title">Image Selected : {{ $image->name }}</h4>
                             <br>
 
                             <div class="row">
@@ -38,36 +39,79 @@
                                     {!! Form::text('envVariables', null, ['class'=>"form-control",
                                         'placeholder' =>"Environment variables (Optional) - Use ';' (semicolon) to separate, Ex: PASSWORD=password;POSTGRES_USER=user;"]) !!}
                                 </div>
+                                <div class="col-sm-10">
+                                    {!! Form::text('Labels', "", ['class'=>"form-control",
+                                        'placeholder' =>"Labels (Optional) - Use ';' (semicolon) to separate, Ex: PASSWORD=password;POSTGRES_USER=user;"]) !!}
+                                </div>
                             </div>
                             <br>
                             <div class="row">
                                 <div class="col-sm-1">
                                     <i class="material-icons">device_hub</i>
                                 </div>
-                                <div class="col-sm-4">
-                                    {!! Form::text('add-host', null, ['class'=>"form-control", 'placeholder' =>"Add a host (Optional)"]) !!}
+                                <div class="col-sm-9">
+                                    {!! Form::text('Hostname', "", ['class'=>"form-control", 'placeholder' =>"Add a Hostname (Optional)"]) !!}
                                 </div>
+                            </div>
+                            <div class="row">
                                 <div class="col-sm-1">
                                     <i class="material-icons">dns</i>
                                 </div>
                                 <div class="col-sm-4">
-                                    {!! Form::text('dns', null, ['class'=>"form-control", 'placeholder' =>"Set custom DNS servers (Optional)"]) !!}
+                                    {!! Form::text('Dns', "", ['class'=>"form-control", 'placeholder' =>"Set custom DNS server (Optional)"]) !!}
+                                </div>
+                                <div class="col-sm-1">
+                                </div>
+                                <div class="col-sm-4">
+                                    {!! Form::text('DnsOptions', "", ['class'=>"form-control", 'placeholder' =>"Set custom DnsOptions (Optional)"]) !!}
                                 </div>
                             </div>
+                            <div class="row">
+                                <div class="col-sm-1">
+                                </div>
+                                <div class="col-sm-4">
+                                    {!! Form::text('DnsSearch', "", ['class'=>"form-control", 'placeholder' =>"Set custom DnsSearch(Optional)"]) !!}
+                                </div>
+                            </div>
+
                             <div class="row">
                                 <div class="col-sm-1">
                                     <i class="material-icons">router</i>
                                 </div>
                                 <div class="col-sm-4">
-                                    {!! Form::text('ip', null, ['class'=>"form-control", 'placeholder' =>"Add a IPv4 (Optional)"]) !!}
+                                    {!! Form::text('IPAddress', null, ['class'=>"form-control", 'placeholder' =>"Add a IPv4 (Optional)"]) !!}
                                 </div>
                             </div>
                             <br>
                             <div class="row">
-                                <div class="col-sm">
-                                    {{ Form::checkbox('external-port', '-P', true, ['id' => 'external-port']) }}
-                                    {{ Form::label('external-port-label', 'Add external communication port', ['for' => 'external-port']) }}
-                                    <a><i class="material-icons">public</i></a>
+                                <div class="col-sm-12">
+                                    <a class = 'col-sm-1'><i class="material-icons">public</i></a>
+                                    {{ Form::label('external-port-label', 'Add external communication port', ['class' => 'col-sm-4', 'for' => '#external-port']) }}
+                                    {{ Form::checkbox('external-port', '-P', true, ['id' => 'external-port', 'class' => 'col-sm-2']) }}
+                                </div>
+                            </div>
+                            <br>
+                            <div class="row">
+                                <div class="col-sm-1">
+                                    <i class="material-icons">router</i>
+                                </div>
+                                <div class="col-sm-4">
+                                    {!! Form::text('MacAddress', "", ['class'=>"form-control", 'placeholder' => "Add a MacAddress (Optional)"]) !!}
+                                </div>
+                                <div class="col-sm-1">
+                                    <i class="material-icons">router</i>
+                                </div>
+                                <div class="col-sm-4">
+                                    {!! Form::text('Memory', 0, ['class'=>"form-control", 'placeholder' => "Add a Memory limite (Optional)"]) !!}
+                                </div>
+                            </div>
+                            <br>
+                            <div class="row">
+                                <div class="col-sm-1">
+                                    <i class="material-icons">router</i>
+                                </div>
+                                <div class="col-sm-9">
+                                    {!! Form::text('NetworkMode', "", ['class'=>"form-control", 'placeholder' => "Network mode. Supported values are: bridge, host, none, and container:<name|id>.(Optional)"]) !!}
                                 </div>
                             </div>
                             <br>
