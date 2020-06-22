59cc09638c7193fa0ff0b84b7a75bfa7bfef3387
saucesar <sau.cesarlima2013@gmail.com>
2020-06-22 13:36:38 -0300

    Adicionada tela de terminal.

--- resources/views/pages/my-containers/my_containers_terminal_tab.blade.php
+++ resources/views/pages/my-containers/my_containers_terminal_tab.blade.php
@@ -23,66 +23,19 @@
 
 @push('js')
 <script>
-var url = "ws://localhost:12345/containers/fd0e74a9d1499e9c58ee0e67/attach/ws?logs=1&stream=1&stdin=1&stdout=1&stderr=1";
-var webSocket = new WebSocket(url);
+const url = "ws://localhost:12345/v1.27/containers/37fa431533f1/attach/ws?logs=1&stream=1&stdin=1&stdout=1&stderr=1";
+//const url = "ws://echo.websocket.org";
+const webSocket = new WebSocket(url);
 
-var attachAddon = new AttachAddon.AttachAddon(webSocket);
-var fitAddon = new FitAddon.FitAddon();
-
-var term = new Terminal();
-
-const prompt = '\r\n\x1B[1;3;32mdocker@docker\x1B[0m $ ';
-var buffer = [];
+const attachAddon = new AttachAddon.AttachAddon(webSocket);
+const fitAddon = new FitAddon.FitAddon();
+const term = new Terminal();
 
 term.loadAddon(attachAddon);
 term.loadAddon(fitAddon);
+
 term.open(document.getElementById('terminal'));
-term.write(prompt);
 fitAddon.fit();
-
-term.prompt = () => {
-    term.write(prompt);
-};
-
-webSocket.onopen = function(evt){
-    alert('Connection open!');
-}
-
-webSocket.onmessage = function(evt){
-    term.write(evt.data);
-    term.prompt();
-}
-webSocket.onclose = function(evt){
-    term.write("\r\nClose connection!");
-    term.prompt();
-}
-
-term.onKey(key => {
-    const ev = key.domEvent;
-    const printable = !ev.altKey && !ev.ctrlKey && !ev.metaKey;
-    if (ev.keyCode === 13) {
-        console.log('enter');
-
-        if(buffer == 'clear'){
-            term.clear();
-        } else {
-            term.write('\r\n');
-            webSocket.send(buffer);
-            console.log('Socket State: ' + webSocket.readyState);
-        }
-        term.prompt();
-        buffer = [];
-    } else if (ev.keyCode === 8) {
-        console.log('backspace');
-
-        buffer = buffer.slice(0, -1);
-        if (term._core.buffer.x > 2) {
-            term.write('\b \b');
-        }
-    } else if (printable) {
-        term.write(key.key);
-        buffer += key.key;
-    }
-});
+term.reset();
 </script>
 @endpush
