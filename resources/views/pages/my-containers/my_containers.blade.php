50ee015191492aa58a67632d801ad52e5c44c472
saucesar <sau.cesarlima2013@gmail.com>
2020-06-25 00:52:59 -0300

    Adicao de botao para download de logs

--- resources/views/pages/my-containers/my_containers.blade.php
+++ resources/views/pages/my-containers/my_containers.blade.php
@@ -14,72 +14,84 @@
                         <p class="card-category">List of Instace Container Images</p>
                     </div>
                     <div class="card-body">
-                        <div class="table-responsive">
-                            @if(session('error'))
-                            <div class="alert alert-danger">{{ session('error') }}</div>
-                            @endif
-                            @if(session('success'))
-                            <div class="alert alert-success">{{ session('success') }}</div>
-                            @endif
-                            <table class='table'>
-                                <thead>
-                                    <th>#</th>
-                                    <th>Container Id</th>
-                                    <th>Nickname</th>
-                                    <th>Iniciated at</th>
-                                    <th>Running</th>
-                                    <th>Options</th>
-                                </thead>
-                                <tbody>
-                                    @foreach ($mycontainers as $container)
-                                    <tr>
-                                        <td><i class="fas fa-box"></i></td>
-                                        <td>{{ substr($container->docker_id, 0, 12) }}</td>
-                                        <td>{{ $container->nickname }}</td>
-                                        <td>{{ $container->dataHora_instanciado }}</td>
-                                        <td class="td-actions text-center">
-                                            @if ($container->dataHora_finalizado)
-                                            <a href="#" class="btn btn-danger" data-original-title="" title="">
-                                                <i class=" material-icons">stop</i>
+                        @if(session('error'))
+                        <div class="alert alert-danger">{{ session('error') }}</div>
+                        @endif
+                        @if(session('success'))
+                        <div class="alert alert-success">{{ session('success') }}</div>
+                        @endif
+                        <table class='table'>
+                            <thead>
+                                <th>#</th>
+                                <th>Container Id</th>
+                                <th>Nickname</th>
+                                <th>Iniciated at</th>
+                                <th>Running</th>
+                                <th>Options</th>
+                            </thead>
+                            <tbody>
+                                @foreach ($mycontainers as $container)
+                                <tr>
+                                    <td><i class="fas fa-box"></i></td>
+                                    <td>{{ substr($container->docker_id, 0, 12) }}</td>
+                                    <td>{{ $container->nickname }}</td>
+                                    <td>{{ $container->dataHora_instanciado }}</td>
+                                    <td class="td-actions text-center">
+                                        @if ($container->dataHora_finalizado)
+                                        <a href="#" class="btn btn-danger" data-original-title="" title="">
+                                            <i class=" material-icons">stop</i>
+                                        </a>
+                                        @else
+                                        <div class="spinner-grow text-success" role="status">
+                                            <span class="sr-only">Loading...</span>
+                                        </div>
+                                        @endif
+                                    </td>
+                                    <td class="td-actions text-right">
+                                        <div class='row'>
+                                            @if($container->dataHora_finalizado)
+                                            <a href="{{ route('instance.playStop', $container->docker_id) }}"
+                                                class="btn btn-link btn-success" data-original-title=""
+                                                title="Play/Pause the container.">
+                                                <i class=" material-icons">play_circle_outline</i>
                                             </a>
                                             @else
-                                            <div class="spinner-grow text-success" role="status">
-                                                <span class="sr-only">Loading...</span>
-                                            </div>
+                                            <a href="{{ route('instance.playStop', $container->docker_id) }}"
+                                                class="btn btn-link btn-warning" data-original-title=""
+                                                title="Play/Pause the container.">
+                                                <i class=" material-icons">pause_circle_outline</i>
+                                            </a>
                                             @endif
-                                        </td>
-                                        <td class="td-actions text-right">
-                                            <div class='row'>
-                                                @if($container->dataHora_finalizado)
-                                                <a href="{{ route('instance.playStop', $container->docker_id) }}" class="btn btn-link btn-success" data-original-title="" title="Play/Pause this container.">
-                                                    <i class=" material-icons">play_circle_outline</i>
-                                                </a>
-                                                @else
-                                                <a href="{{ route('instance.playStop', $container->docker_id) }}" class="btn btn-link btn-warning" data-original-title="" title="Play/Pause this container.">
-                                                    <i class=" material-icons">pause_circle_outline</i>
-                                                </a>
-                                                @endif
-                                                <a href="{{ route('container.terminalTab', $container->docker_id) }}" class="btn btn-info btn-link" target="_black" title="Open terminal to this container.">
-                                                    <i class="fas fa-terminal"></i>
-                                                </a>
-                                                <a href="{{$dockerHost}}/containers/{{$container->docker_id}}/export" class="btn btn-link" title="Download your container.">
-                                                    <i class=" material-icons">get_app</i>
-                                                </a>
-                                                <a href="{{ route('containers.show' , [$container->docker_id]) }}" class="btn btn-link" title="Container details page.">
-                                                    <i class="material-icons">error</i>
-                                                </a>
-                                                {!! Form::open(['route' => ['InstanciaContainers.destroy', $container->docker_id], 'method' => 'delete']) !!}
-                                                <button type="submit" class="btn btn-danger btn-link" title="Detele this container." onclick="return confirm('Are you sure?')" type="submit">
-                                                    <i class="material-icons">delete</i>
-                                                </button>
-                                                {!! Form::close() !!}
-                                            </div>
-                                        </td>
-                                    </tr>
-                                    @endforeach
-                                </tbody>
-                            </table>
-                        </div>
+                                            <a href="{{ route('container.terminalTab', $container->docker_id) }}"
+                                                class="btn btn-info btn-link" target="_black" title="Open terminal.">
+                                                <i class="fas fa-terminal"></i>
+                                            </a>
+                                            <a href="{{$dockerHost}}/containers/{{$container->docker_id}}/export"
+                                                class="btn btn-link" title="Download.">
+                                                <i class=" material-icons">get_app</i>
+                                            </a>
+                                            <a href="{{ route('containers.show' , [$container->docker_id]) }}"
+                                                class="btn btn-link" title="Details.">
+                                                <i class="material-icons">error</i>
+                                            </a>
+                                            <a href="{{$dockerHost}}/containers/{{$container->docker_id}}/logs?timestamps=1&stdout=1&stderr=1"
+                                                class="btn btn-link" title="Logs.">
+                                                <i class="fas fa-file-alt"></i>
+                                            </a>
+                                            {!! Form::open(['route' => ['InstanciaContainers.destroy',
+                                            $container->docker_id], 'method' => 'delete']) !!}
+                                            <button type="submit" class="btn btn-danger btn-link"
+                                                title="Detele the container." onclick="return confirm('Are you sure?')"
+                                                type="submit">
+                                                <i class="material-icons">delete</i>
+                                            </button>
+                                            {!! Form::close() !!}
+                                        </div>
+                                    </td>
+                                </tr>
+                                @endforeach
+                            </tbody>
+                        </table>
                     </div>
                 </div>
             </div>
