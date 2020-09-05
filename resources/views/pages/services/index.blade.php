a260d3a7615575977670b0c943d959a32cd76c41
saucesar <sau.cesarlima2013@gmail.com>
2020-09-04 21:42:35 -0300

    Configurada tabela para exibição de serviçoes.

--- resources/views/pages/services/index.blade.php
+++ resources/views/pages/services/index.blade.php
@@ -5,7 +5,6 @@
 
 @section('content')
 <div class="content">
-<div class="content">
     <div class="container-fluid">
         <div class="row">
             <div class="col-md-12">
@@ -22,40 +21,83 @@
                             @if(session('success'))
                             <div class="alert alert-success">{{ session('success') }}</div>
                             @endif
-
-                            @foreach($services as $service)
-                            <table class="table table-bordered table-hover text-center">
+                            @if(count($services) == 0)
+                                <h3>No Services Found</h3>
+                            @else
+                            <table class="table table-bordered table-hover table-responsive text-center">
                                 <thead class="thead-light">
-                                    <th>Name</th>
-                                    <th>Image</th>
-                                    <th>Replicas</th>
+                                    <tr>
+                                        <th rowspan="2">Name</th>
+                                        <th rowspan="2">Image</th>
+                                        <th rowspan="2">Replicas</th>
+                                        <th colspan="3">Ports</th>
+                                        <th colspan="3" rowspan="2">Options</th>
+                                    </tr>
+                                    <tr>
+                                        <td colspan="1">Protocol</td>
+                                        <td colspan="1">Target</td>
+                                        <td colspan="1">Published</td>
+                                    </tr>
                                 </thead>
+                                @foreach($services as $service)
                                 <tbody>
                                     <tr>
-                                        <td scope="col">{{ $service['Spec']['Name'] }}</td>
-                                        <td scope="col">{{ $service['Spec']['TaskTemplate']['ContainerSpec']['Image'] }}</td>
-                                        <td scope="col">{{ $service['Spec']['Mode']['Replicated']['Replicas'] }}</td>
+                                        <td scope="col"
+                                            rowspan="{{ isset($service['Spec']['EndpointSpec']['Ports']) ? count($service['Spec']['EndpointSpec']['Ports'])*2 : 2 }}">
+                                            {{ $service['Spec']['Name'] }}
+                                        </td>
+                                        <td scope="col"
+                                            rowspan="{{ isset($service['Spec']['EndpointSpec']['Ports']) ? count($service['Spec']['EndpointSpec']['Ports'])*2 : 2 }}">
+                                            {{ $service['Spec']['TaskTemplate']['ContainerSpec']['Image'] }}
+                                        </td>
+                                        <td scope="col"
+                                            rowspan="{{ isset($service['Spec']['EndpointSpec']['Ports']) ? count($service['Spec']['EndpointSpec']['Ports'])*2 : 2 }}">
+                                            {{ $service['Spec']['Mode']['Replicated']['Replicas'] }}
+                                        </td>
+                                        <td scope="col">
+                                            @if(isset($service['Spec']['EndpointSpec']['Ports']))
+                                                @foreach($service['Spec']['EndpointSpec']['Ports'] as $port)
+                                                    {{ $port['Protocol'] }}<br>
+                                                @endforeach
+                                            @endif
+                                        </td><br>
+                                        <td scope="col">
+                                            @if(isset($service['Spec']['EndpointSpec']['Ports']))
+                                                @foreach($service['Spec']['EndpointSpec']['Ports'] as $port)
+                                                    {{ $port['TargetPort'] }}<br>
+                                                @endforeach    
+                                            @endif
+                                        </td>
+                                        <td scope="col">
+                                            @if(isset($service['Spec']['EndpointSpec']['Ports']))
+                                                @foreach($service['Spec']['EndpointSpec']['Ports'] as $port)
+                                                    {{ $port['PublishedPort'] }}<br>
+                                                @endforeach
+                                            @endif
+                                        </td>
+                                        <td>
+                                            <form action="{{ route('services.destroy', $service['ID']) }}" method="post">
+                                                @method('delete')
+                                                @csrf
+                                                <button type="submit" class="btn btn-sm btn-primary btn-link" onclick="return confirm('Are you sure? This action not be undone.');">
+                                                    <i class="material-icons">delete</i>
+                                                </button>
+                                            </form>
+                                        </td>
                                     </tr>
-                                    <tr>
-                                        <td colspan="3">Ports</td>
-                                    </tr>
-                                    <tr>
-                                        <td>Protocol</td>
-                                        <td>Target</td>
-                                        <td>Published</td>
-                                    </tr>
-                                    @foreach($service['Spec']['EndpointSpec']['Ports'] as $port)
-                                    <tr>
-                                        <td scope="col">{{ $port['Protocol'] }}</td>
-                                        <td scope="col">{{ $port['TargetPort'] }}</td>
-                                        <td scope="col">{{ $port['PublishedPort'] }}</td>
-                                    </tr>
-                                    @endforeach
                                 </tbody>
+                                @endforeach
                             </table>
-                            @endforeach
+                            @endif
                         </div>
                     </div>
+                </div>
+                <div class="col-lg-11 text-right" style="margin-left: 48px;">
+                    <button class="btn btn-primary btn-fab btn-round" title="Create a service.">
+                        <a href="{{ route('services.create') }}">
+                            <i class="material-icons" style="color:white">add_to_queue</i>
+                        </a>
+                    </button>
                 </div>
             </div>
         </div>
