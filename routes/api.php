27beab41c656354d6142de1926de1c041f42f04b
saucesar <saucesarlima2013@gmail.com>
2020-09-09 00:17:05 -0300

    Rotas de containers realocadas para web.

--- routes/api.php
+++ routes/api.php
@@ -19,5 +19,3 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 });
 
 Route::apiResource('atividadeMaquinas', 'Api\AtividadeMaquinasController');
-Route::apiResource('containers', 'Api\ContainersController')->except(['create', 'index', 'show']);
-Route::get('InstanciaContainers/stop/{containerId}', 'Api\ContainersController@playStop')->name('instance.playStop');
