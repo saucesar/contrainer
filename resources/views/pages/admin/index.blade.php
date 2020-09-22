980a856a888e9dbccb2fe8af1b6f3361601f1d1d
saucesar <saucesarlima2013@gmail.com>
2020-09-21 23:26:38 -0300

    Erro de sintaxe js corrigido .

--- resources/views/pages/admin/index.blade.php
+++ resources/views/pages/admin/index.blade.php
@@ -138,9 +138,6 @@
 
 @push('js')
 <script>
-    var
-</script>
-<script>
 $(document).ready(function() {
     // Javascript method's body can be found in assets/js/demos.js
     md.initDashboardPageCharts();
