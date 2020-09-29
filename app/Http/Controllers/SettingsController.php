87c1417fbab9637986e1bfafc067ee57e275e9e2
saucesar <saucesarlima2013@gmail.com>
2020-09-29 20:45:59 -0300

    Atualização de template de serviços funcionando.

--- app/Http/Controllers/SettingsController.php
+++ app/Http/Controllers/SettingsController.php
@@ -23,6 +23,23 @@ class SettingsController extends Controller
 
     public function updateServiceTemplate(Request $request)
     {
-        dd($request->all());
+        $service_template = json_decode(DB::table('default_templates')->where('name', 'service')->first()->template, true);
+        
+        $service_template['TaskTemplate']['ContainerSpec']['DNSConfig']['Nameservers'] = explode(';', $request->dnsNameservers);
+        $service_template['TaskTemplate']['ContainerSpec']['TTY'] = isset($request->tty);
+        $service_template['TaskTemplate']['ContainerSpec']['OpenStdin'] = isset($request->openStdin);
+        $service_template['TaskTemplate']['Resources']['Limits']['MemoryBytes'] = $request->memoryBytes * 1024 * 1024;
+        $service_template['TaskTemplate']['RestartPolicy']['Condition'] = $request->restartCondition;
+        $service_template['TaskTemplate']['RestartPolicy']['Delay'] = intval($request->restartDelay);
+        $service_template['TaskTemplate']['RestartPolicy']['MaxAttempts'] = intval($request->restartMax);
+        $service_template['Mode']['Replicated']['Replicas'] = intval($request->replicas);
+        $service_template['UpdateConfig']['FailureAction'] = $request->failureAction;
+        $service_template['UpdateConfig']['Monitor'] = intval($request->updateMonitor);
+        $service_template['UpdateConfig']['MaxFailureRatio'] = intval($request->maxFailureRatio);
+        $service_template['UpdateConfig']['Order'] = $request->updateOrder;
+
+        DB::table('default_templates')->where('name', 'service')->update(['template' => json_encode($service_template)]);
+
+        return back()->with('success', 'Service Template Updated!');
     }
 }
