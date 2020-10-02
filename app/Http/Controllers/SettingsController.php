03bb8f496f2acfdd9bacd2c6f83ab6d6f9f385ca
saucesar <saucesarlima2013@gmail.com>
2020-10-02 01:38:41 -0300

    Adição de parametros para exibição correta de labels.

--- app/Http/Controllers/SettingsController.php
+++ app/Http/Controllers/SettingsController.php
@@ -11,14 +11,29 @@ class SettingsController extends Controller
     {
         $serv = DB::table('default_templates')->where('name', 'service')->first()->template;
         $cont = DB::table('default_templates')->where('name', 'container')->first()->template;
+        $cont_decode = json_decode($cont, true);
+        $serv_decode = json_decode($serv, true);
+
         $params = [
-            'service_template' => json_decode($serv, true),
-            'container_template' => json_decode($cont, true),
+            'service_template' => $serv_decode,
+            'container_template' => $cont_decode,
             'service_template_json' => $serv,
             'container_template_json' => $cont,
+            'cont_labels' => $this->getLabels($cont_decode['Labels']),
+            'serv_labels' => $this->getLabels($serv_decode['Labels']),
         ];
 
         return view('pages/settings/index', $params);
+    }
+
+    private function getLabels($array)
+    {
+        $labels = '';
+        
+        foreach(array_keys($array) as $key){
+            $labels = $labels.$key.':'.$array[$key].';';
+        }
+        return $labels;
     }
 
     public function phpinfo()
@@ -46,5 +61,14 @@ class SettingsController extends Controller
         DB::table('default_templates')->where('name', 'service')->update(['template' => json_encode($service_template)]);
 
         return back()->with('success', 'Service Template Updated!');
+    }
+
+    public function updateContainerTemplate(Request $request)
+    {
+        $container_template = json_decode(DB::table('default_templates')->where('name', 'container')->first()->template, true);
+
+        DB::table('default_templates')->where('name', 'container')->update(['template' => json_encode($container_template)]);
+
+        return back()->with('success', 'Container Template Updated!');
     }
 }
