0e0760a16e166e3461ad7d0e29c0053f296ebe20
saucesar <saucesarlima2013@gmail.com>
2020-10-03 02:55:00 -0300

    Incremento no tamanho do modal.

--- resources/views/pages/settings/modal_containers_template.blade.php
+++ resources/views/pages/settings/modal_containers_template.blade.php
@@ -1,5 +1,5 @@
 <div class="modal" id="modalContainersService" tabindex="-1" role="dialog" aria-labelledby="modalContainersLabel" aria-hidden="true">
-    <div class="modal-dialog" role="document">
+    <div class="modal-dialog modal-lg" role="document">
         <div class="modal-content">
             <div class="modal-header">
                 <h5 class="modal-title" id="modalContainersLabel">Change default template to create containers</h5>
--- resources/views/pages/settings/modal_services_template.blade.php
+++ resources/views/pages/settings/modal_services_template.blade.php
@@ -1,5 +1,5 @@
 <div class="modal" id="modalServices" tabindex="-1" role="dialog" aria-labelledby="modalServicesLabel" aria-hidden="true">
-    <div class="modal-dialog" role="document">
+    <div class="modal-dialog modal-lg" role="document">
         <div class="modal-content">
             <div class="modal-header">
                 <h5 class="modal-title" id="modalServicesLabel">Change default template to create services</h5>
