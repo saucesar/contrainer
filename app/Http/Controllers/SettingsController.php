05fa07652d715a0279d238cf32512d1f10f28640
saucesar <saucesarlima2013@gmail.com>
2020-10-02 23:48:59 -0300

    função de para atualizar template padrão de containers.

--- app/Http/Controllers/SettingsController.php
+++ app/Http/Controllers/SettingsController.php
@@ -19,14 +19,14 @@ class SettingsController extends Controller
             'container_template' => $cont_decode,
             'service_template_json' => $serv,
             'container_template_json' => $cont,
-            'cont_labels' => $this->getLabels($cont_decode['Labels']),
-            'serv_labels' => $this->getLabels($serv_decode['Labels']),
+            'cont_labels' => $this->toStringLabels($cont_decode['Labels']),
+            'serv_labels' => $this->toStringLabels($serv_decode['Labels']),
         ];
 
         return view('pages/settings/index', $params);
     }
 
-    private function getLabels($array)
+    private function toStringLabels($array)
     {
         $labels = '';
         
@@ -67,8 +67,51 @@ class SettingsController extends Controller
     {
         $container_template = json_decode(DB::table('default_templates')->where('name', 'container')->first()->template, true);
 
+        $container_template['Domainname'] = str_replace(' ', '', $request->Domainname);
+        $container_template['Labels'] = $this->labelsToArray($request->Labels);
+        $container_template['Dns'] = $this->removeBlanck(explode(';', $request->dns));
+        $container_template['DnsOptions'] = $this->removeBlanck(explode(';', $request->dnsOptions));
+        $container_template['IPAddress'] = $request->IPAddress;
+        $container_template['IPPrefixLen'] = intval($request->IPPrefixLen);
+        $container_template['MacAddress'] = $request->MacAddress;
+        $container_template['Memory'] = $request->Memory;
+        $container_template['Env'] = $this->removeBlanck(explode(';', $request->env));
+        $container_template['AttachStdin'] = isset($request->AttachStdin);
+        $container_template['AttachStdout'] = isset($request->AttachStdout);
+        $container_template['AttachStderr'] = isset($request->AttachStderr);
+        $container_template['OpenStdin'] = isset($request->OpenStdin);
+        $container_template['StdinOnce'] = isset($request->StdinOnce);
+        $container_template['Tty'] = isset($request->Tty);
+        $container_template['HostConfig']['PublishAllPorts'] = isset($request->PublishAllPorts);
+        $container_template['HostConfig']['Privileged'] = isset($request->Privileged);
+        $container_template['NetworkMode'] = $request->NetworkMode;
+        $container_template['Entrypoint'] = $this->removeBlanck(explode(';', $request->Entrypoint));
+        $container_template['HostConfig']['RestartPolicy']['name'] = $request->RestartPolicy;
+        $container_template['HostConfig']['Binds'] = $this->removeBlanck(explode(';', $request->Binds));
+        $container_template['HostConfig']['NetworkMode'] = $request->NetworkMode;
+
         DB::table('default_templates')->where('name', 'container')->update(['template' => json_encode($container_template)]);
 
         return back()->with('success', 'Container Template Updated!');
+    }
+
+    private function removeBlanck($array)
+    {
+        return $array[count($array)-1] == '' ? array_diff($array, ['']) : $array;
+    }
+
+    private function labelsToArray($labels)
+    {
+        $labels_array = [];
+        $array = explode(';', $labels);
+
+        foreach($array as $label){
+            if($label != ''){
+                $temp = explode(':', $label);
+                $labels_array[$temp[0]] = $temp[1];
+            }
+        }
+
+        return $labels_array;
     }
 }
