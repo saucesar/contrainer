7ea0c0ee2f634e5620b7014474e496799c42c0a3
saucesar <saucesarlima2013@gmail.com>
2020-10-07 21:31:12 -0300

    Model  e migration para registro de serviços

---
+++ database/migrations/2020_10_07_141730_create_services_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateServicesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('services', function (Blueprint $table) {
+            $table->id();
+            $table->string('docker_id')->unique();
+            $table->integer('user_id');
+            $table->string('name', 256)->unique();
+            $table->integer('port');
+
+            $table->foreign('user_id')->references('id')->on('users');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('services');
+    }
+}
