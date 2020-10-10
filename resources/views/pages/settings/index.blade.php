3afc1c0b1f129a532594a189986c6f7414d152f7
saucesar <saucesarlima2013@gmail.com>
2020-10-10 18:21:26 -0300

    adição de modal para crud de user categories.

--- resources/views/pages/settings/index.blade.php
+++ resources/views/pages/settings/index.blade.php
@@ -25,6 +25,7 @@ jsonViewerContainer.showJSON(<?= $container_template_json ?>, -1, 1);
             </div>
         </div>
         <div class="row">
+
             <div class="col-6">
                 <div class="card">
                     <div class="card-header card-header-primary">
@@ -46,6 +47,7 @@ jsonViewerContainer.showJSON(<?= $container_template_json ?>, -1, 1);
                     </div>
                 </div>
             </div>
+
             <div class="col-6">
                 <div class="card">
                     <div class="card-header card-header-primary">
@@ -67,9 +69,80 @@ jsonViewerContainer.showJSON(<?= $container_template_json ?>, -1, 1);
                     </div>
                 </div>
             </div>
+
+            <div class="col-6">
+                <div class="card">
+                    <div class="card-header card-header-primary">
+                        <h4 class="card-title ">User Categories</h4>
+                        <p class="card-category"><strong>Configure resource limits for each user category</strong></p>
+                    </div>
+                    <div class="card-body table table-responsive">
+                        <div class="row">
+                            <div class="col overflow-auto" style="height: 250px;">
+                                <table class="table table-hover">
+                                <thead class="thead-dark">
+                                    <th>#</th>
+                                    <th>Name</th>
+                                    <th>RAM Limit</th>
+                                    <th>Storage Limit</th>
+                                    <th>Options</th>
+                                </thead>
+                                <tbody>
+                                @foreach($categories as $category)
+                                <tr>
+                                    <td>{{ $category->id }}</td>
+                                    <td>{{ $category->name }}</td>
+                                    <td>{{ $category->ram_limit }} MB</td>
+                                    <td>{{ $category->storage_limit }} MB</td>
+                                    <td class="td-actions">
+                                        <div class="btn-group" role="group" aria-label="Basic example">
+                                            <button class="btn btn-sm btn-link btn-warning" data-toggle="modal"
+                                                    data-target="#modalUserCategory{{ $category->id }}" title="Edit category">
+                                                    <i class="material-icons">edit</i>
+                                            </button>
+                                            <form action="{{ route('user-categories.destroy', $category->id) }}" method="post">
+                                                @csrf
+                                                @method('delete')
+                                                <button class="btn btn-sm btn-link btn-danger" type="submit" onclick="return confirm('are you sure?');">
+                                                    <i class="material-icons">delete</i>
+                                                </button>
+                                            </form>
+                                        </div>
+                                    </td>
+                                </tr>
+                                @endforeach
+                                </tbody>
+                            </table>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="card-footer">
+                        <button class="btn btn-success btn-link" data-toggle="modal"
+                                data-target="#modalUserCategoryNew" title="New Category">
+                                <i class="material-icons">add_circle_outline</i>
+                        </button>
+                    </div>
+                </div>
+            </div>
+
         </div>
         @include('pages.settings.modal_services_template')
         @include('pages.settings.modal_containers_template')
+
+        @foreach($categories as $category)
+            @include('pages.settings.modal_user_category', [
+                'route' => route('user-categories.update', $category->id),
+                'category' => $category,
+                'method' => 'PUT',
+            ])
+        @endforeach
+
+        @include('pages.settings.modal_user_category', [
+            'route' => route('user-categories.store'),
+            'id' => 'New',
+            'category' => null,
+            'method' => 'POST',
+        ])
     </div>
 </div>
 @endsection
