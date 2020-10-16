5ba853630132b5a8f7221025cde2d112b48c2425
saucesar <saucesarlima2013@gmail.com>
2020-10-16 14:09:59 -0300

    funções separadas em trait para facilitar reuso.

---
+++ app/Http/Controllers/traits/ArrayTrait.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\Controllers\traits;
+
+trait ArrayTrait {
+
+    public function extractArray($keys, $values, $separator = '=', $upper = false)
+    {
+        $array = [];
+
+        for($i = 0; $i < count($keys); $i++){
+            if(isset($keys[$i]) && $values[$i]){
+                $val = ($upper ? strtoupper($keys[$i]) : $keys[$i]).$separator.$values[$i];
+                $array[] = $val;
+            }
+        }
+
+        return $array;
+    }
+
+    public function extractLabels($request)
+    {
+        $labelKeys = $request->LabelKeys;
+        $labelValues = $request->LabelValues;
+        
+        $labels = [];
+
+        for($i = 0; $i < count($labelKeys); $i++){
+            if(isset($labelKeys[$i]) && isset($labelValues[$i])){
+                $labels[$labelKeys[$i]] = $labelValues[$i];
+            }
+        }
+        
+        return $labels;
+    }
+
+    public function removeNull($array, $index = 0)
+    {
+        unset($array[$index]);
+        return $array;
+    }
+}
