5614dc43b5faa1932ae748af13a3a97364296812
saucesar <sau.cesarlima2013@gmail.com>
2020-11-09 21:14:17 -0300

    metodo de atualização de volume driver.

--- app/Http/Controllers/SettingsController.php
+++ app/Http/Controllers/SettingsController.php
@@ -13,17 +13,21 @@ class SettingsController extends Controller
     
     public function index()
     {
+        $volume_driver = DB::table('default_templates')->where('name', 'volume_driver')->first()->template;
         $serv = DB::table('default_templates')->where('name', 'service')->first()->template;
         $cont = DB::table('default_templates')->where('name', 'container')->first()->template;
         $cont_decode = json_decode($cont, true);
         $serv_decode = json_decode($serv, true);
-
+        $volume_driver_decode = json_decode($volume_driver, true);
+        //dd($volume_driver_decode);
         $params = [
             'categories' => UserCategory::all(),
+            'volume_driver' => $volume_driver_decode,
             'service_template' => $serv_decode,
             'container_template' => $cont_decode,
             'service_template_json' => $serv,
             'container_template_json' => $cont,
+            'volume_driver_json' => $volume_driver,
             'cont_labels' => $this->toStringLabels($cont_decode['Labels']),
             'serv_labels' => $this->toStringLabels($serv_decode['Labels']),
         ];
@@ -81,7 +85,7 @@ class SettingsController extends Controller
         $container_template['IPAddress'] = $request->IPAddress;
         $container_template['IPPrefixLen'] = intval($request->IPPrefixLen);
         $container_template['Memory'] = intval($request->Memory);
-        $container_template['Env'] = $this->extractArray($request->EnvKeys, $request->EnvValues, '=',true);
+        $container_template['Env'] = $this->extractArray($request->EnvKeys, $request->EnvValues, '=', true);
         $container_template['AttachStdin'] = isset($request->AttachStdin);
         $container_template['AttachStdout'] = isset($request->AttachStdout);
         $container_template['AttachStderr'] = isset($request->AttachStderr);
@@ -99,6 +103,19 @@ class SettingsController extends Controller
         DB::table('default_templates')->where('name', 'container')->update(['template' => json_encode($container_template)]);
 
         return back()->with('success', 'Container Template Updated!');
+    }
+
+    public function updateVolumeDriver(Request $request)
+    {
+        $volume_driver = json_decode(DB::table('default_templates')->where('name', 'volume_driver')->first()->template, true);
+
+        $volume_driver['Driver'] = $request->driver_name;
+        $volume_driver['DriverOpts'] = $this->extractArrayKey($request->OptKeys, $request->OptValues);
+        //dd($volume_driver);
+
+        DB::table('default_templates')->where('name', 'volume_driver')->update(['template' => json_encode($volume_driver)]);
+
+        return back()->with('success', 'Volume Driver Updated!');
     }
 
     private function rules()
